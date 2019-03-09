251c3b27a9d9e89ad08271e90d7d108c406e78e1
Jake Bills <jakebills1@gmail.com>
2019-03-09 10:30:42 -0700

    add model associations and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users 
+  
+  resources :users, only: [:show] do 
+    resources :trips, except: [:index, :show, :new, :edit]
+  end
+
+  resources :trips, only: [:show] do
+    resources :locations, except: [:index, :show, :new, :edit] do
+      resources :addresses, except: [:index, :show, :new, :edit]
+    end
+  end
+
+  # create"locations/:location_id/adresses" => "addresses#create"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
