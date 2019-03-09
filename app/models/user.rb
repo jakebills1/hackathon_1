b9d4a7f9e266cc967e8f3b728bc4ddba42dbe583
Jake Bills <jakebills1@gmail.com>
2019-03-09 12:28:59 -0700

    pre lunch

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :trips, dependent: :destroy
+  def full_name
+    first_name +  " " + last_name
+  end
 end
